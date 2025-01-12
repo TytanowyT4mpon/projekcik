205db20c7831261fff93397de1e3b811a5bd1323
krzysiu <fak1223@gmail.com>
2025-01-12 19:23:48 +0100

    build and actualities

--- src/app/page.js
+++ src/app/page.js
@@ -43,6 +43,45 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="section1">
+
+      <div className="bld">
+                <h2>ZBUDUJ<br/> SWOJĄ <br/>LISTĘ</h2>
+                <p>Stwórz swoją listę gier, oznaczaj ograne <br/> tytuły i udostępnij swoją bibliotekę. Bądź na <br/>bieżąco w świecie gier.</p>
+                <a class="joinus">Dołącz do nas<span class=""><svg width="30" height="16" viewBox="0 0 30 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9L1 7ZM29.7071 8.70711C30.0976 8.31658 30.0976 7.68342 29.7071 7.29289L23.3431 0.928932C22.9526 0.538408 22.3195 0.538408 21.9289 0.928932C21.5384 1.31946 21.5384 1.95262 21.9289 2.34315L27.5858 8L21.9289 13.6569C21.5384 14.0474 21.5384 14.6805 21.9289 15.0711C22.3195 15.4616 22.9526 15.4616 23.3431 15.0711L29.7071 8.70711ZM1 9L29 9V7L1 7L1 9Z" fill="url(#paint0_linear_301_81)"/>
+<defs>
+<linearGradient id="paint0_linear_301_81" x1="2.5" y1="7.5" x2="31" y2="10.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#8F00FF"/>
+<stop offset="1" stop-color="#BA62FF"/>
+</linearGradient>
+</defs>
+</svg></span></a>
+                </div>
+
+        <div className="pad">
+          <img src="pad.png"></img>
+        </div>
+
+              
+                <div className="act">
+                  <h2>BĄDŹ <br/> NA <br/> BIEŻĄCO</h2>
+                <div className="icons">
+                      <div className="icon1">
+                        <img src="/icon1.png"></img>
+                      </div>
+                      <div className="icon2">
+                      <img src="/icon2.png"></img>
+                      </div>
+                      <div className="icon3">
+                      <img src="/icon3.png"></img>
+                      </div>
+                    </div>
+                </div>
+        
+
+      </div>
+
     
       <div className="section2">
           <h1>NIE CZEKAJ <br/>ZBUDUJ LISTĘ <br/>JUŻ DZIŚ</h1>
